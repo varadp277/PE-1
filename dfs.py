473e0be0714ed2a26f0c4d5bc4817245484dcf93
varadp277 <varadpatil7763@gmail.com>
2025-08-13 10:48:56 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,21 @@
+# DFS using recursion
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited = set()
+
+def dfs(node):
+    if node not in visited:
+        print(node, end=' ')
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(neighbor)
+
+print("DFS Traversal:")
+dfs('A')
